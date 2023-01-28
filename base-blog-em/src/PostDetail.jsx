0be0f36c974c76cdae354bef0bf0a431c79bbb10
blackmoja <limlim980625@gmail.com>
2023-01-29 02:57:25 +0900

    feat: useQuery 적용

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,17 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const postId = post.id;
+  const { data, error, isLoading, isError } = useQuery(["post", postId], () =>
+    fetchComments(postId)
+  );
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    <>
+      <h3>Error...</h3>
+      <p>{error.toString()}</p>
+    </>;
 
   return (
     <>
